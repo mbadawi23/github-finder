564b4108e701750d460139bb58e04d0efddd9df5
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-15 22:46:25 -0800

    using async componentDidMount to get users from github.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 class App extends Component {
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users');
+    console.log(res.data);
+  }
+
   render() {
     return (
       <div className='App'>
