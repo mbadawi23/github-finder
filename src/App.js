2fcd4214fec7cac673b9ba85f575ecdda89bd5de
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-15 22:59:04 -0800

    Setting app.state to users.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
   async componentDidMount() {
+    this.setState({ loading: true });
+
     const res = await axios.get('https://api.github.com/users');
-    console.log(res.data);
+
+    this.setState({ users: res.data, loading: false });
   }
 
   render() {
