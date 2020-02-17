abaa53cdced4cbbe55acd8047b2efdc6109ca5cb
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-16 22:20:12 -0800

    replaced fetch by componentDidMount with searchUsers

--- src/App.js
+++ src/App.js
@@ -11,21 +11,17 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  // Search Github users
+  // Because this is an arrow func, async comes before the parameter.
+  searchUsers = async text => {
     this.setState({ loading: true });
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+
+    console.log(text);
     const res = await axios.get(
-      `https://api.github.com/users?
-      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ users: res.data, loading: false });
-  }
-
-  // Search Github users
-  searchUsers = text => {
-    console.log(text);
+    this.setState({ users: res.data.items, loading: false });
   };
 
   render() {
