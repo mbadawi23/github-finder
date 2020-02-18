573193a6a21806ea4e0698de1d7de4bbbbf2a5f1
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-18 00:20:16 -0800

    Added conditional bio

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -55,6 +55,14 @@ export class User extends Component {
             <h1>{name}</h1>
             <p>Location: {location}</p>
           </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Biography</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+          </div>
         </div>
       </Fragment>
     );
