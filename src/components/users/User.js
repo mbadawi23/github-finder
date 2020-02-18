9cc5c41e3c67902be1fd793215a2b34fc1c52d33
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-18 00:16:11 -0800

    Added avatar img with styling.

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -44,6 +44,16 @@ export class User extends Component {
         ) : (
           <i className='fas fa-times-circle text-danger' />
         )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              className='round-img'
+              style={{ width: '150px' }}
+              alt='avatar'
+            />
+          </div>
+        </div>
       </Fragment>
     );
   }
