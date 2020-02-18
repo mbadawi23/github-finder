f788dd046e880824b82ed35dd9b06bf152ed929c
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-17 23:55:08 -0800

    Added proptypes

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Spinner from '../layout/Spinner';
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+
+  static propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
   render() {
     const {
       name,
