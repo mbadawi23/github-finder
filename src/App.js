2210c9923c4971c433d6d2efd9bdea3b94c60ec2
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-17 23:49:31 -0800

    Bug fix: Another url bug, those are tricky.

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
 
     console.log(username);
     const res = await axios.get(
-      `https://api.github.com/search/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({ user: res.data, loading: false });
